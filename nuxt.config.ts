408f18474b8a166270f623121a00cc30ca421d20
Aleksandra Tusinska <tusinska.aleksandra@gmail.com>
2026-05-22 13:19:47 +0200

    Pre-bundle 2 dependencies to avoid page reloads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,4 +31,12 @@ export default defineNuxtConfig({
       },
     },
   },
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit',
+      ]
+    }
+  },
 });

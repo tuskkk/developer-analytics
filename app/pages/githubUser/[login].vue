cc2193081827e304b23c0cc93721ad625108dee7
Aleksandra Tusinska <tusinska.aleksandra@gmail.com>
2026-08-07 13:41:09 +0200

    Revert "Test: template"
    This reverts commit 37ba89bceab15a86873421ea01cf63f78b09e00e.

--- app/pages/githubUser/[login].vue
+++ app/pages/githubUser/[login].vue
@@ -1,5 +1,5 @@
 <template>
-  <!--<AppPage banner-image="developer">
+  <AppPage banner-image="developer">
     <template #content>
       <div v-if="userError" class="w-full h-screen pt-32">
         <ErrorBanner
@@ -25,8 +25,7 @@
         No user found with login "{{ login }}"
       </div>
     </template>
-  </AppPage>-->
-  <div>Hello SSR</div>
+  </AppPage>
 </template>
 
 <script setup lang="ts">
